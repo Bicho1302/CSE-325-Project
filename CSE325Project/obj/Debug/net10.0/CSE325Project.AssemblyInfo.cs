b67e897fc11fd4ea4124923fdcfb050871c71fb4
Bicho1302 <victorsn13022003@gmail.com>
2026-06-13 15:24:40 -0600

    Added create and join study group features

--- CSE325Project/obj/Debug/net10.0/CSE325Project.AssemblyInfo.cs
+++ CSE325Project/obj/Debug/net10.0/CSE325Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSE325Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dda84527105927513e45058dce48e770a0af6d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05c8bb7913f57089943ab83a78781007ff05cc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSE325Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSE325Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
