09e7741d43a898f8663f0901533228509a2ead23
Bicho1302 <victorsn13022003@gmail.com>
2026-06-17 19:45:53 -0600

    Added Dockerfile for deployment

--- CSE325Project/obj/Debug/net10.0/CSE325Project.AssemblyInfo.cs
+++ CSE325Project/obj/Debug/net10.0/CSE325Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSE325Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e099fafed0145c7b9e5199db00febb27379099")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f6e9923959fce4dfa2dd2582abe5f8ccd639b8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSE325Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSE325Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
